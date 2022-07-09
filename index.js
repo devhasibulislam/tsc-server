170536811282dfa6bd22b2192e5ae3b07b8ec1f3
hasibulislam999 <hasib143sl@gmail.com>
2022-07-09 13:45:35 +0600

    express initialization with node done

---
+++ index.js
@@ -0,0 +1,22 @@
+// required middleware
+const express = require('express');
+var cors = require('cors')
+const bodyParser = require('body-parser');
+require("dotenv").config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// connect middleware
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// enable requests
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
