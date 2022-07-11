7fc0ba6c708a43b47abceb541f750d68909bbd42
hasibulislam999 <hasib143sl@gmail.com>
2022-07-11 19:13:17 +0600

    teacher & student count document segment done

--- index.js
+++ index.js
@@ -50,6 +50,10 @@ async function run() {
             res.send(await classCollection.find({}).toArray());
         })
 
+        /**
+         * TEACHER segment
+         */
+
         // add new teacher
         app.post('/teacher', async (req, res) => {
             res.send(await teacherCollection.insertOne(req.body));
@@ -70,6 +74,15 @@ async function run() {
             res.send(await teacherCollection.updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body }, { upsert: true }));
         })
 
+        // count total teachers
+        app.get('/teachersCount', async (req, res) => {
+            res.send({ totalTeachers: await teacherCollection.estimatedDocumentCount() });
+        })
+
+        /**
+         * STUDENT segment
+         */
+
         // add new student
         app.post('/student', async (req, res) => {
             res.send(await studentCollection.insertOne(req.body));
@@ -77,7 +90,7 @@ async function run() {
 
         // get all students
         app.get('/students', async (req, res) => {
-            res.send(await studentCollection.find({}).toArray());
+            res.send(await studentCollection.find({}).skip(parseInt(req.query.page) * 5).limit(5).toArray());
         })
 
         // delete a student
@@ -88,6 +101,11 @@ async function run() {
         // update a student
         app.put('/student/:id', async (req, res) => {
             res.send(await studentCollection.updateOne({ _id: ObjectId(req.params.id) }, { $set: req.body }, { upsert: true }));
+        })
+
+        // count total students
+        app.get('/studentsCount', async (req, res) => {
+            res.send({ totalStudents: await studentCollection.estimatedDocumentCount() });
         })
     } finally {
         // await database.close();
